c71a1d94a7fcbbc96edb2d98a664f8296d4ca482
Caio <caiotorresbr@gmail.com>
2024-07-28 14:23:36 -0400

    latest updates

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -2,24 +2,30 @@ const buttonSearch = document.getElementById('searchBtn')
 const destinations = []
 const searchResultDiv = document.getElementById('searchResult');
 
-function search() {
-    // retrieves input and converts to lowercase
-    const destinationSearch = document.getElementById('destinationInput').value.toLowerCase();
 
-
-    
-}
 
 function recommendationResults() {
+
+    function search() {
+    // retrieves input and converts to lowercase
+    const destinationSearch = document.getElementById('destinationInput').value.toLowerCase();
+    destinationSearch.innerHTML = "";
+
+    
     // fetch api data
     fetch('travel_recommendation_api.json')
 
-        if (searchResultDiv === ) {}
-
-        .then (response => response.json());
+        .then (response => response.json())
         // handles response in json format
         .then (data => {
             // 
-            const = data.
+            const destination = data.destination.find(item => item.countries.toLowerCase() === destinationSearch)
         }
     )
+}
+}
+
+    function resetSearch() {
+        document.getElementById("searchResult").value = ""
+        document.getElementById("destinationInput").value = ""
+    }
