8ad2b5e7f1c5c401edce2fbce7b74a8fc5a7cae0
Caio <caiotorresbr@gmail.com>
2024-08-05 12:07:33 -0400

    js updates

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -9,9 +9,8 @@ function recommendationResults() {
 
     function search() {
     // retrieves input and converts to lowercase
-    const destinationSearch = document.getElementById('destinationInput').value.toLowerCase();
-    destinationSearch.innerHTML = "";
-
+        const destinationSearch = document.getElementById('destinationInput').value.toLowerCase();
+        destinationSearch.innerHTML = "";
     
     // fetch api data
     fetch('travel_recommendation_api.json')
@@ -20,15 +19,31 @@ function recommendationResults() {
         // handles response in json format
         .then (data => {
             
-            const country = data.destination.find(item => item.countries.name.toLowerCase()  === destinationSearch )
-            const temple = data.destination.find(item => item.temples.name.toLowerCase() === destinationSearch ) 
-            const beach = data.destination.find(item => item.beaches.name.toLowerCse() === destinationSearch )
-            
-            
+            // search keywords and their match on json document
+            const country = data.destination.find(item => item.countries.name.toLowerCase()  === destinationSearch );
+            const temple = data.destination.find(item => item.temples.name.toLowerCase() === destinationSearch );
+            const beach = data.destination.find(item => item.beaches.name.toLowerCase() === destinationSearch );
+
+
+
+            if (country) {
+                searchResultDiv.innerHTML += 
+                `<h2>Meet ${country.name}!</h2>
+                <h3>Cities</h3>
+                <h4>${country.name.cities.name[0]}</h4>
+                <img src="${country.name.cities.imageUrl[0]}">
+                <p>${country.name.cities.description[0]}</p>`
+            }
+        })
+
+        .catch(error => {
+           console.error("Error: ", error);
+           searchResultDiv.innerHTML += "An error occured during the fecthing process" 
+        })
+
         }
-    )
-}
-}
+        }
+
 
     function resetSearch() {
         document.getElementById("searchResult").value = ""
@@ -36,5 +51,5 @@ function recommendationResults() {
     }
 
 
-searchBtn.addEventListener("click", search);
+searchBtn.addEventListener("click", recommendationResults);
 resetBtn.addEventListener("click", resetSearch);
